da98d70d3c23f08494f6ecea9d6a2ed26279a79f
Daniel Correa Lobato <daniel@lobato.org>
2024-09-28 02:10:23 -0300

    Terceiro exemplo de EF

---
+++ ProdutoDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EF.Exemplo3;
+
+public class ProdutoDbContext : DbContext
+{
+    public DbSet<Produto> tblProdutos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseNpgsql(@"Host=10.0.1.95;" +
+                                 "Username=netuser;" +
+                                 "Password=netpassword;" +
+                                 "Database=exemplo3");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using EF.Exemplo3;
+
+using var db = new ProdutoDbContext();
+if (!db.tblProdutos.Any())
+{
+    Console.WriteLine("Seeeding...");
+    var novosDados = new List<Produto>
+    {
+        new() {
+            ProdutoNome = "Refrigerante Guaraná Antarctica Zero Açúcar 200ml",
+            ProdutoPreco = 1.43m, ProdutoEstoque = 1, ProdutoAtivo = true },
+        new() {
+            ProdutoNome = "Água Tônica Zero Açúcar Schweppes Lata 220ml",
+            ProdutoPreco = 1.99m, ProdutoEstoque = 1, ProdutoAtivo = true },
+        new() {
+            ProdutoNome = "Suco Pronto Maguary Laranja 1L",
+            ProdutoPreco = 5.70m, ProdutoEstoque = 1, ProdutoAtivo = true },
+        new() {
+            ProdutoNome = "Linguiça Toscana Aurora Pacote 800g",
+            ProdutoPreco = 13.94m, ProdutoEstoque = 1, ProdutoAtivo = true },
+        new() {
+            ProdutoNome = "Manteiga Aviação com Sal 500g Pote",
+            ProdutoPreco = 32.65m, ProdutoEstoque = 1, ProdutoAtivo = true },
+        new() {
+            ProdutoNome = "Amaciante Ypê Aconchego 2 Litros",
+            ProdutoPreco = 9.59m, ProdutoEstoque = 1, ProdutoAtivo = true },
+    };
+    db.AddRange(novosDados);
+    db.SaveChanges();
+    Console.WriteLine("Done!");
+}
